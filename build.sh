c00c0268dfb9d7dd839f1c0f8c9f700f518358f8
raventhecat333 <jdmyers2016@outlook.com>
2021-09-04 12:25:37 -0300

    changing build names

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # This is a comment!
-makerom -rsf homebrew.rsf -elf lpp-3ds.elf -icon icon.icn -banner banner.bnr -f cia -o cookie.cia
+makerom -rsf cookie.rsf -elf lpp-3ds.elf -icon icon.icn -banner banner.bnr -f cia -o cookie.cia
 echo done
